b9363b7da0adb8dd140864ce9c8fb4a1620005f5
Mia Borja <apk.borja@gmail.com>
2022-07-02 18:10:38 -0700

    Removed unnecessary model

--- models/Thought.js
+++ models/Thought.js
@@ -3,18 +3,20 @@ const { Schema, Types } = require('mongoose');
 const thoughtSchema = new Schema(
     {
         thoughtText: {
-
+            type: String,
+            required: true,
+            min_length: 1,
+            max_length: 280,
         },
         createdAt: {
-
+            type: Date,
+            default: Date.now,
         },
         username: {
-
-        },
-        reactions: [{
             type: Schema.Types.ObjectId,
-            ref: 'reaction',
-        }],
+            ref: 'user',
+        },
+        reactions: [reactionSchema],
     },
     {
         toJSON: {
@@ -24,7 +26,7 @@ const thoughtSchema = new Schema(
     },
 );
 
-thoughtSchema.virtual('reaction').get(function () {
+thoughtSchema.virtual('reactionCount').get(function () {
     return this.reactions.length;
 });
 
