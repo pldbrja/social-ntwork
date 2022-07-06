6d1afb7ca1d2b9b9f34cdc1bfb7ea7219c843ae5
Mia Borja <apk.borja@gmail.com>
2022-07-05 22:51:49 -0700

    Updated user and thought controllers

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -2,24 +2,24 @@ const { User, Thought } = require('../models');
 
 module.exports = {
     getThoughts(req, res) {
-
+        Thought.find();
     },
     getThought(req, res) {
-
+        Thought.findOne();
     },
     createThought(req, res) {
-
+        Thought.create();
     },
     updateThought(req, res) {
-
+        Thought.findOneandUpdate();
     },
     deleteThought(req, res) {
-
+        Thought.findOneandDelete();
     },
     createReaction(req, res) {
-
+        Thought.findOneandUpdate();
     },
     deleteReaction(req, res) {
-
+        Thought.findOneandUpdate();
     },
 };
--- controllers/userController.js
+++ controllers/userController.js
@@ -3,23 +3,38 @@ const { User, Thought } = require('../models');
 module.exports = {
     getUsers(req, res) {
         User.find()
+            .then(async (users) => {
+
+            })
+            .catch((err) => {
+                console.log(err);
+                return res.status(500).json(err);
+            });
     },
     getUser(req, res) {
-        User.findOne()
+        User.findOne({ _id: req.params.userId })
+            .select('-__v')
+            .then(async (student) => {
+
+            })
+            .catch((err) => {
+                console.log(err);
+                return res.status(500).json(err);
+            });
     },
     createUser(req, res) {
-        User.create()
+        User.create();
     },
     updateUser(req, res) {
-
+        User.findOneAndUpdate();
     },
     deleteUser(req, res) {
-
+        User.findOneAndDelete();
     },
     addFriend(req, res) {
-
+        User.findOneAndUpdate();
     },
     deleteFriend(req, res) {
-        
+        User.findOneandUpdate();
     },
 };
